ae77083de1b9d09e11f9430b689872b0a592ccdd
mempron <mempron@outlook.com>
2022-08-21 01:38:51 +0300

    Removed useless lines of code which can raise an exception.

--- aiobot/handlers/user.py
+++ aiobot/handlers/user.py
@@ -1,16 +1,10 @@
 from aiogram.dispatcher.router import Router
 from aiogram.filters import CommandStart
 from aiogram import types
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from ..models.user import User
 
 user_router = Router()
 
 
 @user_router.message(CommandStart())
-async def start_handler(message: types.Message, session: AsyncSession) -> None:
-
-    session.add(User(message.from_user.id))
-
-    await message.answer(f'Привет, <b>{message.from_user.full_name}!</b>\nЯ создал запись о тебе в моей таблице.')
+async def start_handler(message: types.Message) -> None:
+    await message.answer(f'Привет, <b>{message.from_user.full_name}!</b>')
